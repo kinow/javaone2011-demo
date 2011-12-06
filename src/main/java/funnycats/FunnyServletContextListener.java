084da2b74707cbad906d88d39a94c627bf2aa543
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2011-12-05 22:14:20 -0200

    Added a test for Utils

--- src/main/java/funnycats/FunnyServletContextListener.java
+++ src/main/java/funnycats/FunnyServletContextListener.java
@@ -7,19 +7,30 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FunnyServletContextListener implements ServletContextListener {
 
+	private Configuration configuration = null;
+	
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(FunnyServletContextListener.class);
 
 	public void contextInitialized(ServletContextEvent event) {
 		LOGGER.info("*** Starting up FunnyCats application ***");
 
+		try {
+			this.configuration = new PropertiesConfiguration("funnycats.properties");
+		} catch (ConfigurationException e) {
+			throw new RuntimeException(e);
+		}
+		
 		ServletContext ctx = event.getServletContext();
-		String path = ctx.getRealPath("/resources/cats/");
+		String path = ctx.getRealPath(configuration.getString("cats.pictures"));
 		File directory = new File(path);
 
 		List<FunnyCat> funnyCats = Utils.getFunnyCats(directory);
@@ -31,6 +42,7 @@ public class FunnyServletContextListener implements ServletContextListener {
 
 	public void contextDestroyed(ServletContextEvent event) {
 		LOGGER.info("*** Shutting down FunnyCats application ***");
+		this.configuration = null;
 	}
 
 }
